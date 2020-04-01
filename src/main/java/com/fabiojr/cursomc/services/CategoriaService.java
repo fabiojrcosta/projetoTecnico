58383314393884259bb8588de789c0359c10d269
Fabio Jr. Costa <fabiojr.sweet@gmail.com>
2020-04-01 10:14:53 -0300

    Criando operação de instanciacao

--- src/main/java/com/fabiojr/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabiojr/cursomc/services/CategoriaService.java
@@ -10,13 +10,13 @@ import com.fabiojr.cursomc.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-		
-		public Categoria find(Integer id) {
-			Optional<Categoria> obj = repo.findById(id);
-			return obj.orElse(null);
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
 	}
 
 }

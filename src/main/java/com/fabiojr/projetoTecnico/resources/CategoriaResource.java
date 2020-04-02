3fab142fec17608eb6eb051d55586ca406ced3eb
Fabio Jr. Costa <fabiojr.sweet@gmail.com>
2020-04-02 17:01:02 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/fabiojr/projetoTecnico/resources/CategoriaResource.java
+++ src/main/java/com/fabiojr/projetoTecnico/resources/CategoriaResource.java
@@ -3,9 +3,11 @@ package com.fabiojr.projetoTecnico.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabiojr.projetoTecnico.domain.Categoria;
 import com.fabiojr.projetoTecnico.services.CategoriaService;
@@ -24,4 +26,13 @@ public class CategoriaResource {
 
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		java.net.URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+
+	}
 }

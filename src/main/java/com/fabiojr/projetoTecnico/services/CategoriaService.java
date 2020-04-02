f78febf8a1fdfc09b09f9935ebd5f9e6929e5631
Fabio Jr. Costa <fabiojr.sweet@gmail.com>
2020-04-02 20:12:54 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/fabiojr/projetoTecnico/services/CategoriaService.java
+++ src/main/java/com/fabiojr/projetoTecnico/services/CategoriaService.java
@@ -20,8 +20,14 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+
+	public Categoria update(Categoria obj) {
+		return repo.save(obj);
+	}
+
 }

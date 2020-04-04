36fc84d48c2d47bd66ba7a1f68573d3463231a77
Fabio Jr. Costa <fabiojr.sweet@gmail.com>
2020-04-04 12:04:29 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/fabiojr/projetoTecnico/services/CategoriaService.java
+++ src/main/java/com/fabiojr/projetoTecnico/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabiojr.projetoTecnico.domain.Categoria;
@@ -47,5 +50,14 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-
+	
+	public Page<Categoria> findPage(Integer page, Integer LinesPerPage,String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, LinesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+		
 }
+	
+
+

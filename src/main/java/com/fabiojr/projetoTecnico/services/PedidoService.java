49e8014b0c4c2c38c3791a215bc27df949e9c77e
Fabio Jr. Costa <fabiojr.sweet@gmail.com>
2020-04-07 21:57:07 -0300

    Implementando toString de Pedido

--- src/main/java/com/fabiojr/projetoTecnico/services/PedidoService.java
+++ src/main/java/com/fabiojr/projetoTecnico/services/PedidoService.java
@@ -32,6 +32,9 @@ public class PedidoService {
 
 	@Autowired
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 
 
@@ -55,10 +58,20 @@ public class PedidoService {
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
-			ip.setPedido(obj);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
+	}
+
+	public ClienteService getClienteService() {
+		return clienteService;
+	}
+
+	public void setClienteService(ClienteService clienteService) {
+		this.clienteService = clienteService;
 	}
 
 }
